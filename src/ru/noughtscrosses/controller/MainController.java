67cb6d6cd95b1d141389cff3c56abd70dd76f2a7
Pavel Uspenskij <uspenskiydj@gmail.com>
2019-09-15 10:12:22 +0400

    Initial commit.

---
+++ src/ru/noughtscrosses/controller/MainController.java
@@ -0,0 +1,17 @@
+package ru.noughtscrosses.controller;
+
+import java.util.Random;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class MainController {   
+    
+}
---
+++ src/ru/noughtscrosses/start/Main.java
@@ -0,0 +1,26 @@
+package ru.noughtscrosses.start;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/ru/noughtscrosses/fxml/main.fxml"));
+        Scene scene = new Scene(root, 1000, 700);
+        
+        stage.setTitle("Крестики-нолики");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
